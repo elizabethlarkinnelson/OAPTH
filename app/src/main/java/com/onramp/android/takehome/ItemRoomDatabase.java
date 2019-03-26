735930e1076dcf60d44aaaa14e347a76161651a2
Elizabeth Nelson <17551572+elizabethlarkinnelson@users.noreply.github.com>
2019-03-26 15:43:17 -0700

    Got viewmodel working.  To figure out how to use it with fragment, next

--- app/src/main/java/com/onramp/android/takehome/ItemRoomDatabase.java
+++ app/src/main/java/com/onramp/android/takehome/ItemRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.onramp.android.takehome;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = {Item.class}, version = 1, exportSchema = false)
 public abstract class ItemRoomDatabase extends RoomDatabase {
@@ -21,10 +24,41 @@ public abstract class ItemRoomDatabase extends RoomDatabase {
                             ItemRoomDatabase.class,
                             "item_database")
                             .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onOpen (@NonNull SupportSQLiteDatabase db){
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void>{
+
+        private final ItemDao mDao;
+        String[] items = {"mocha", "latte", "iced team"};
+
+        PopulateDbAsync(ItemRoomDatabase db){
+            mDao = db.itemDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params){
+            mDao.deleteAll();
+
+            for (int i = 0; i <= items.length - 1; i++){
+                Item item = new Item(items[i]);
+                mDao.insert(item);
+            }
+
+            return null;
+        }
     }
 }
--- app/src/main/java/com/onramp/android/takehome/OrderActivity.java
+++ app/src/main/java/com/onramp/android/takehome/OrderActivity.java
@@ -1,8 +1,11 @@
 package com.onramp.android.takehome;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,17 +13,30 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.List;
+
 public class OrderActivity extends AppCompatActivity{
+
+    private ItemViewModel mItemViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order);
 
+        mItemViewModel = ViewModelProviders.of(this).get(ItemViewModel.class);
+
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         final ItemListAdapter adapter = new ItemListAdapter(this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mItemViewModel.getAllItems().observe(this, new Observer<List<Item>>(){
+            @Override
+            public void onChanged(@Nullable final List<Item> items){
+                adapter.setItems(items);
+            }
+        });
 
 //        getSupportFragmentManager().beginTransaction().replace(R.id.order_container, new OrderFragment()).commit();
     }
